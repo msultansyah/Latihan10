bbde54613a640eb97390e1528038a4d46eae7584
msultansyah <msultansyah11@gmail.com>
2020-01-31 18:44:01 +0800

    Initial commit

---
+++ app/src/main/java/id/ac/poliban/mi/sultan/latihan10/SubActivity.java
@@ -0,0 +1,29 @@
+package id.ac.poliban.mi.sultan.latihan10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+        EditText etYourName = findViewById(R.id.etName);
+        Button btFinish = findViewById(R.id.btFinish);
+
+        btFinish.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            String data = etYourName.getText().toString();
+            intent.putExtra("subactivity", data);
+            setResult(Activity.RESULT_OK, intent);
+            finish();
+        });
+    }
+}
